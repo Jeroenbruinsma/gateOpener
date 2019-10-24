9c414e4552fa2036f11fbaa89275b594df6e4a5d
Jeroen <Jeroenbruinsma@gmail.com>
2019-10-24 17:14:42 +0200

    add hardware logic

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const {port} = require('./constants')
 const app = express()
 const jsonParser = bodyParser.json()
 
+//raspberry io config
+const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+const gateRelay = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+
+
 app.use(jsonParser)
 
 app.listen(port , () => console.log("Server running on port", port) )
@@ -17,7 +22,18 @@ app.get('/', (request, response) => {
  
 app.post('/gate', (request, response) => {
     console.log("got an post request on /gate")
+    openGate()
     response.status(200)
     response.send({status: "GATE_OPEN"})
 
 })
+
+const openGate = () => {
+    gateRelay.writeSync(1)
+    setTimeout(gateRelay.writeSync(0), 3000); //close  gateRelais after 3 seconds 
+    // gateRelay.unexport(); //don't know if we need this. // Unexport GPIO to free resources
+}
+
+
+
+
