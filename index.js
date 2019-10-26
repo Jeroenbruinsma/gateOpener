3628ba28b40238c8c736b9f22f66365ff9651d35
Jeroen <Jeroenbruinsma@gmail.com>
2019-10-26 17:12:26 +0200

    tryout timeout

--- index.js
+++ index.js
@@ -30,7 +30,13 @@ app.post('/gate', (request, response) => {
 
 const openGate = () => {
     gateRelay.writeSync(1)
-    setTimeout(gateRelay.writeSync(0), 3000); //close  gateRelais after 3 seconds 
+    //setTimeout(closeGate, 3000); //close  gateRelais after 3 seconds 
+    setTimeout(function(){ console.log("Hello"); }, 3000);
+    // gateRelay.unexport(); //don't know if we need this. // Unexport GPIO to free resources
+}
+
+const closeGate = () => {
+    gateRelay.writeSync(0)
     // gateRelay.unexport(); //don't know if we need this. // Unexport GPIO to free resources
 }
 
